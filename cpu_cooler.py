403c057bea9319cd65567d02af2e884dd01b7eb9
Renan Kaic Lopes <renan-kaic@outlook.com>
2025-10-14 12:19:30 -0300

    Ref: Remove byte print

--- cpu_cooler.py
+++ cpu_cooler.py
@@ -19,7 +19,6 @@ def write_to_cpu_fan_display(dev):
     # GAMEMAX SIGMA 520 CPU cooler requires the number 1 instead of 0 to be sent when writing to the display
     byte_comands = bytes([1, int(fCpuTemp)])
     
-    print(f"Byte commands: {byte_comands}")
     try:        
         num_bytes_written = dev.write(byte_comands)
     except IOError as e:
