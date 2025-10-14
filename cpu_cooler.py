abb73855dd0738a023b2307e2d848817da03ce3f
Renan Kaic Lopes <renan-kaic@outlook.com>
2025-10-14 12:04:43 -0300

    Feat: Adding support for Gamemax Sigma 520 CPU Cooler

--- cpu_cooler.py
+++ cpu_cooler.py
@@ -1,22 +1,26 @@
-import hid
+import hid, pprint
 from threading import Event, Thread
 import psutil
 
 def get_cpu_temp():
-    temp = psutil.sensors_temperatures()['k10temp'][0].current
+    #pprint.pprint(psutil.sensors_temperatures()['coretemp'])    
+    temp = psutil.sensors_temperatures()['coretemp'][0].current    
     return temp
 
-VENDOR_ID = 0xaa88
-PRODUCT_ID = 0x8666
+# GAMEMAX SIGMA 520 DEVICE
+VENDOR_ID = 0x5131
+PRODUCT_ID = 0x2007
 
 device = hid.Device(VENDOR_ID, PRODUCT_ID)
 
 def write_to_cpu_fan_display(dev):
     fCpuTemp = get_cpu_temp()
 
-    byte_comands = bytes([0, int(fCpuTemp)])
-
-    try:
+    # GAMEMAX SIGMA 520 CPU cooler requires the number 1 instead of 0 to be sent when writing to the display
+    byte_comands = bytes([1, int(fCpuTemp)])
+    
+    print(f"Byte commands: {byte_comands}")
+    try:        
         num_bytes_written = dev.write(byte_comands)
     except IOError as e:
         print ('Error writing command: {}'.format(e))
@@ -35,4 +39,4 @@ def call_repeatedly(interval, func, *args):
 print('Connected to {}\n'.format(PRODUCT_ID))
 
 seconds = 1
-cancel_future_calls = call_repeatedly(seconds, write_to_cpu_fan_display, device)
+cancel_future_calls = call_repeatedly(seconds, write_to_cpu_fan_display, device)1
